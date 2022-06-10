f3a208e21d26d02ae6d2c1ac8f255077943796ea
RobertaGS <roberta.girulyte@gmail.com>
2022-06-10 11:06:31 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,39 @@
+let counterOne = 0;
+setInterval(() => {
+    const percentageNumber = document.querySelector('.numberOne');
+    const percentageNumberValue = Number(percentageNumber.getAttribute('data-value'));
+    if (counterOne !== percentageNumberValue) {
+        counterOne++;
+        percentageNumber.innerHTML = `${counterOne}%`;
+    }
+}, 90);
+
+let counterTwo = 0;
+setInterval(() => {
+    const percentageNumber = document.querySelector('.numberTwo');
+    const percentageNumberValue = Number(percentageNumber.getAttribute('data-value'));
+    if (counterTwo !== percentageNumberValue) {
+        counterTwo++;
+        percentageNumber.innerHTML = `${counterTwo}%`;
+    }
+}, 90);
+
+let counterThree = 0;
+setInterval(() => {
+const percentageNumber = document.querySelector('.numberThree');
+const percentageNumberValue = Number(percentageNumber.getAttribute('data-value'));
+    if (counterThree !== percentageNumberValue) {
+        counterThree++;
+        percentageNumber.innerHTML = `${counterThree}%`;
+         }
+}, 70);
+
+let counterFour = 0;
+setInterval(() => {
+const percentageNumber = document.querySelector('.numberFour');
+const percentageNumberValue = Number(percentageNumber.getAttribute('data-value'));
+    if (counterFour !== percentageNumberValue) {
+        counterFour++;
+        percentageNumber.innerHTML = `${counterFour}%`;
+          }
+},85);
